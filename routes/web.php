adc7b432298c70286eabd773af189ed616668fa8
Júlio Alves <julio.a.valenca@gmail.com>
2021-08-26 09:53:24 -0300

    admin product page

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
@@ -49,6 +50,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function() {
 
 //Routes for admin
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function() {
+    Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
