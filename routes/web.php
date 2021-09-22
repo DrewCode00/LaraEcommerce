a8b8a54cbebb9769c2a9e8a3ab76a7d9057edf31
Júlio Alves <julio.a.valenca@gmail.com>
2021-09-22 10:37:05 -0300

    add the function move the wishlist from the cart

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
+use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -44,6 +45,8 @@ Route::get('/products/{slug}', DetailsComponent::class)->name('product.details')
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
 
 Route::get('/search', SearchComponent::class)->name('product.search');
+
+Route::get('/wishlist', WishlistComponent::class)->name('wishlist');
 
 //Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     //return view('dashboard');
